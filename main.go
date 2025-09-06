28a756cd69e1e4b29bcf4c33887e2120a2b684ce
quin47 <cassia02200059@gmail.com>
2025-09-06 22:40:49 +0800

    fix url

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	fooHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		key := r.URL.Query()["key"][0]
-		base64Str := client.GetSubscription("https://dler.cloud/subscribe/" + key + "mu=trojan&ace=c&lv=1|2|3&type=all")
+		base64Str := client.GetSubscription("https://dler.cloud/subscribe/" + key + "?mu=trojan&ace=c&lv=1|2|3&type=all")
 		w.Write([]byte(base64Str))
 	})
 
